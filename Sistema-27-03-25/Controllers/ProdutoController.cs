43b50128dd1a3fe58f74e6da7040c3b90c850d48
Kevin-C-Silva <Kevin.silva158@etec.sp.gov.br>
2025-03-27 14:53:53 -0300

    Configurando os controllers

---
+++ Sistema-27-03-25/Controllers/ProdutoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistema_27_03_25.Models;
+using Sistema_27_03_25.Repositorio;
+using System.Diagnostics;
+
+namespace Sistema_27_03_25.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ProdutoRepositorio _produtoRepositorio;
+
+        public ProdutoController(ProdutoRepositorio produtoRepositorio)
+        {
+            _produtoRepositorio = produtoRepositorio;
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                _produtoRepositorio.AdicionarUsuario(produto);
+                return RedirectToAction("Login");
+            }
+            return View(produto);
+        }
+    }
+}
---
+++ Sistema-27-03-25/Controllers/UsuarioController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistema_27_03_25.Models;
+using Sistema_27_03_25.Repositorio;
+using System.Diagnostics;
+
+namespace Sistema_27_03_25.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly ILogger<UsuarioController> _logger;
+
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+            if (usuario != null && usuario.Senha == senha)
+            {
+                //Autenticação bem-sucedida
+                // return RedirectToAction("Index","Home"); a caso esteja errado ter como trocar rapido.
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError("", "Email ou senhas inválidos.");
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepositorio.AdicionarUsuario(usuario);
+                return RedirectToAction("Login");
+            }
+            return View(usuario);
+        }
+    }
+}
