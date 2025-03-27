e2533fee7d30118492105d97e4d13e1aeefe3dbf
Kevin-C-Silva <Kevin.silva158@etec.sp.gov.br>
2025-03-27 14:36:49 -0300

    Configurando os repositórios UsuarioRepositorio e ProdutoRepositorio

--- Sistema-27-03-25/Repositorio/ProdutoRepositorio.cs
+++ Sistema-27-03-25/Repositorio/ProdutoRepositorio.cs
@@ -1,6 +1,54 @@
-﻿namespace Sistema_27_03_25.Repositorio
+﻿using Sistema_27_03_25.Models;
+
+namespace Sistema_27_03_25.Repositorio
 {
     public class ProdutoRepositorio
     {
+        private readonly string _connectionString;
+
+        public ProdutoRepositorio(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public void AdicionarUsuario(Produto produto)
+        {
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.MySqlCommand();
+                cmd.CommandText = "INSERT INTO Produto (Nome, Descricao, Preco) VALUES (@Nome,@Descricao)";
+                cmd.Parameters.AddWithValue("@Nome", produto.Nome);
+                cmd.Parameters.AddWithValue("@Descricao", produto.Descricao);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+        public Produto ObterUsuario(string email)
+        {
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.MySqlCommand();
+                cmd.CommandText = "SELECT * FROM Produto";
+                cmd.ExecuteNonQuery();
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new Produto
+                        {
+                            Id = reader.GetInt32("Id"),
+                            Nome = reader.GetString("Nome"),
+                            Descricao = reader.GetString("Descricao"),
+                            Preco = reader.GetDecimal("Preco"),
+
+                        };
+
+                    }                    
+                }
+                return null;
+            }
+        }
+
     }
 }
