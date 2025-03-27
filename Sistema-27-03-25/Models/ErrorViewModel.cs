e37517e40171c3d166fe08d63fd27a09de54a116
Kevin-C-Silva <Kevin.silva158@etec.sp.gov.br>
2025-03-27 13:59:03 -0300

    Criando o Projeto Sistema-27-03-25

---
+++ Sistema-27-03-25/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sistema_27_03_25.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
